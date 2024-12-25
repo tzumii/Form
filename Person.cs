2e1f78267f55b7df92d4f0bc64300f1eaf4da63c
idanka2010 <idan.frolov@gmail.com>
2024-12-25 10:49:11 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp5
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression(@".*@.*", ErrorMessage = "Input must include an '@' symbol.")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
