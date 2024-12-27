1b22ae404d30c2e2d0f013061f18ba91b04d0832
omri tzuman <omritzuman21@gmail.com>
2024-12-27 14:34:08 +0200

    changes

--- Person.cs
+++ Person.cs
@@ -7,6 +7,13 @@ namespace BlazorApp5
         [Required(ErrorMessage = "Email is required.")]
         [RegularExpression(@".*@.*", ErrorMessage = "Input must include an '@' symbol.")]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
